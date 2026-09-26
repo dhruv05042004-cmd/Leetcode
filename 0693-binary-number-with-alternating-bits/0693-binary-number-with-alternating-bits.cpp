2981038e97907e6a80e2f7bf3bd8f5d87befd808
dhruv05042004-cmd <dhruv05042004@gmail.com>
2026-09-26 23:00:21 +0530

    Time: 0 ms (100%), Space: 7.8 MB (27.5%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        while(n>0)
+        {
+            int bit1=n&1;
+            n=n>>1;
+            int bit2=n&1;
+            if(bit1==bit2)return false;
+        }
+        return true;
+        
+    }
+};
